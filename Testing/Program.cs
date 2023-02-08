a598af19dfc20fb9b242df110b4f5a9ab239fe8e
Vaibhav Arora <vaibhav.arora@nagarro.com>
2023-02-08 14:57:32 +0530

    first commit

---
+++ Testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
