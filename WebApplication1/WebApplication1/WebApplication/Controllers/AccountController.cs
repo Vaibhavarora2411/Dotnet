cb0b054f581f3bb9878f3e8512af132ce897fcdb
Vaibhav Arora <vaibhav.arora@nagarro.com>
2023-02-13 16:24:03 +0530

    new file add

---
+++ WebApplication1/WebApplication1/WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+using System.Web.Security;
+
+namespace WebApplication.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Events", "Employees");
+                }
+
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using(var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/WebApplication/Models/Membership.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Membership
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public String Password { get; set; }
+    }
+}
